be03bfbafd2bf3b0660246967d511dfd0bbfeef9
Aayush Rijal <arijal@aiims.com.au>
2023-10-20 19:49:59 +1100

    Project Initialized

---
+++ app/README.md
@@ -0,0 +1,22 @@
+# Landing page [Spotless Carpet Cleaning Perth](http://www.spotlesscarpetsperth.com.au/)
+
+This is a landing page boilerplate that uses PHP scripting language
+
+## _Author Information_
+
+Developer: Aayush Rijal
+
+Contact: 0452637371
+
+[Github](https://github.com/aayushrijal91/spotlesscarpetsperth)
+
+[Figma](https://www.figma.com/file/MapwUnb6RwjJ1DZalqVRZe/Spotless-Carpet-Cleaning-Perth?node-id=45%3A25390&mode=dev)
+
+## _Instruction_
+
+```bash
+ npm install
+ gulp watch
+ ```
+
+Reference for full repository
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{html,js,php}"],
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1442px',
+      '2xl': '1600px',
+    },
+    container: {
+      center: true,
+      padding: '12px',
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'primary': '#ED3D3D',
+        'secondary': '#acce31',
+        'light': '#f0f0f0',
+      },
+      fontFamily: {
+        'proxima-nova': ['Proxima Nova', 'sans-serif'],
+        'grandmas-television': ['Grandmas Television', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
