2b824ed0e4b3df25e7873154625b47df192a72c3
Aayush Rijal <arijal@aiims.com.au>
2023-10-26 16:18:12 +1100

    Aiims logo removed

--- app/src/footer.php
+++ app/src/footer.php
@@ -1,9 +1,9 @@
 <footer class="bg-dark py-4">
 	<div class="container">
-		<div class="flex flex-wrap items-center justify-center md:justify-between gap-y-5">
-			<div class="w-fit">
+		<div class="flex flex-wrap items-center justify-center gap-y-5">
+			<!-- <div class="w-fit">
 				<a href="https://www.aiims.com.au/this-is-us/" target="_blank"><?= renderImg('aiims.png', 'logo') ?></a>
-			</div>
+			</div> -->
 
 			<div class="w-full md:w-fit">
 				<p class="capitalize text-gray-500 font-medium text-xs md:text-sm lg:text-base text-center">Copyright <?= date('Y') ?> <span class="px-2 md:px-3 lg:px-6">|</span> <?= $site ?> <span class="px-2 md:px-3 lg:px-6">|</span> All Rights Reserved</p>
